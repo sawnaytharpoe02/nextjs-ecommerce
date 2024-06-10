2595c030c0b8c0fbbfeafaec068d3381abccf6dc
sawnaytharpoe02 <sawnaytharpoe14@gmail.com>
2024-06-10 23:55:02 +0630

    in progress email templating

---
+++ src/email/PurchaseReceiptEmailTemplate.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Html,
+  Preview,
+  Tailwind,
+} from "@react-email/components";
+import { OrderInformation } from "./components/OrderInformation";
+
+type PurchaseReceiptEmailTemplateProps = {
+  product: {
+    name: string;
+    imagePath: string;
+    description: string;
+  };
+  order: { id: string; createdAt: Date; pricePaidInCents: number };
+  downloadVerificationId: string;
+};
+
+const PurchaseReceiptEmailTemplate = ({
+  product,
+  order,
+  downloadVerificationId,
+}: PurchaseReceiptEmailTemplateProps) => {
+  return (
+    <Html>
+      <Preview>Download {product.name} and view receipt</Preview>
+      <Tailwind>
+        <Head />
+        <Body className="font-sans bg-white">
+          <Container className="max-w-xl">
+            <Heading>Purchase Receipt</Heading>
+            <OrderInformation
+              order={order}
+              product={product}
+              downloadVerificationId={downloadVerificationId}
+            />
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+};
+
+PurchaseReceiptEmailTemplate.PreviewProps = {
+  product: {
+    name: "Product name",
+    description: "Some description",
+    imagePath: "/products/fab21d5a-9683-461c-8862-960210297130-pp7.png",
+  },
+  order: {
+    id: crypto.randomUUID(),
+    createdAt: new Date(),
+    pricePaidInCents: 10000,
+  },
+  downloadVerificationId: crypto.randomUUID(),
+} satisfies PurchaseReceiptEmailTemplateProps;
+
+export default PurchaseReceiptEmailTemplate;
